abf0c945efc6ae9212902f003bf7ed0f84bd87ab
obarker94 <me@oliverbarker.io>
2023-03-09 01:46:38 +0000

    websocket worker remade

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,35 +1,117 @@
-use actix::clock::Instant;
-use actix::{Actor, StreamHandler};
-use actix_web::web::Data;
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use crate::lobby::Lobby;
+use crate::messages::{ClientActorMessage, Connect, Disconnect, WsMessage};
+use actix::{fut, ActorContext, ActorFuture, ContextFutureSpawner, WrapFuture};
+use actix::{Actor, Addr, Running, StreamHandler};
+use actix::{AsyncContext, Handler};
 use actix_web_actors::ws;
-
-use std::collections::HashMap;
+use actix_web_actors::ws::Message::Text;
+use std::time::{Duration, Instant};
 use uuid::Uuid;
 
-pub struct Ws;
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
-impl Actor for Ws {
-    type Context = ws::WebsocketContext<Self>;
+pub struct WsConn {
+    room: Uuid,
+    lobby_addr: Addr<Lobby>,
+    hb: Instant,
+    id: Uuid,
 }
 
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Ws {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
+impl WsConn {
+    pub fn new(room: Uuid, lobby: Addr<Lobby>) -> WsConn {
+        WsConn {
+            id: Uuid::new_v4(),
+            room,
+            lobby_addr: lobby,
+            hb: Instant::now(),
         }
     }
 }
 
-pub async fn websocket_router(
-    req: HttpRequest,
-    stream: web::Payload,
-) -> Result<HttpResponse, Error> {
-    let resp = ws::start(Ws {}, &req, stream);
-    println!("{:?}", resp);
-    resp
+impl Actor for WsConn {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.hb(ctx);
+
+        let addr = ctx.address();
+        self.lobby_addr
+            .send(Connect {
+                addr: addr.recipient(),
+                lobby_id: self.room,
+                self_id: self.id,
+            })
+            .into_actor(self)
+            .then(|res, _, ctx| {
+                match res {
+                    Ok(_res) => (),
+                    _ => ctx.stop(),
+                }
+                fut::ready(())
+            })
+            .wait(ctx);
+    }
+
+    fn stopping(&mut self, _: &mut Self::Context) -> Running {
+        self.lobby_addr.do_send(Disconnect {
+            id: self.id,
+            room_id: self.room,
+        });
+        Running::Stop
+    }
+}
+
+impl WsConn {
+    fn hb(&self, ctx: &mut ws::WebsocketContext<Self>) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                println!("Disconnecting due to failed heartbeat");
+                act.lobby_addr.do_send(Disconnect {
+                    id: act.id,
+                    room_id: act.room,
+                });
+                ctx.stop();
+                return;
+            }
+            ctx.ping(b"PING");
+        });
+    }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsConn {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            Ok(ws::Message::Pong(_)) => {
+                self.hb = Instant::now();
+            }
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(reason)) => {
+                ctx.close(reason);
+                ctx.stop();
+            }
+            Ok(ws::Message::Continuation(_)) => {
+                ctx.stop();
+            }
+            Ok(ws::Message::Nop) => (),
+            Ok(Text(s)) => self.lobby_addr.do_send(ClientActorMessage {
+                id: self.id,
+                msg: s,
+                room_id: self.room,
+            }),
+            Err(e) => panic!("Error: {:?}", e),
+        }
+    }
+}
+
+impl Handler<WsMessage> for WsConn {
+    type Result = ();
+
+    fn handle(&mut self, msg: WsMessage, ctx: &mut Self::Context) {
+        ctx.text(msg.0);
+    }
 }
