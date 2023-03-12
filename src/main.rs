c4349a24c3b6ce052f0b91dc6d1de6d77712382c
obarker94 <me@oliverbarker.io>
2023-03-12 17:09:37 +0000

    rebuilding again with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
-mod lobby;
-mod websocket;
-use lobby::Lobby;
-mod messages;
-mod start_connection;
-use actix::Actor;
-use start_connection::start_connection as start_connection_route;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
 
-use actix_web::{App, HttpServer};
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let chat_server = Lobby::default().start(); //create and spin up a lobby
+#[tokio::main]
+async fn main() {
+    println!("Connection starting!");
+    // create a TCP Listener and bind to an address
+    // we will await this until it is read to accept connection.
+    // unwrap() will panic if it cannot bind - i.e. the port is aleady in use.
+    let listener = TcpListener::bind("127.0.0.1:1338").await.unwrap();
 
-    HttpServer::new(move || {
-        App::new()
-            .service(start_connection_route) //register our route. rename with "as" import or naming conflict
-            .data(chat_server.clone()) //register the lobby
-    })
-    .bind("0.0.0.0:6969")?
-    .run()
-    .await
+    loop {
+        // we destructure the socket and address from the listener after the connection is accepted.
+        // the connection may not be accepted if the listener is not ready to accept connections.
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            // split the socket into a reader and writer.
+            let (reader, mut writer) = socket.split();
+
+            // we create a buffer to store the incoming data.
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            println!("Connection established! {}", _addr);
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                // we write the whole buffer to the socket and unwrap the result. it could panic if the socket is not ready to write.
+                //
+                // i.e. outgoing
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
 }
