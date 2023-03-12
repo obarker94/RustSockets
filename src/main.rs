3aa5880969a8abcad1ab727ce9452006e9b1bf3a
obarker94 <me@oliverbarker.io>
2023-03-12 18:41:57 +0000

    lobby list is now passing to the socket connecitons

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,45 @@
-use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
-    sync::broadcast,
+use std::{
+    collections::HashMap,
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread::spawn,
 };
 
-#[tokio::main]
-async fn main() {
-    println!("Connection starting!");
-    // create a TCP Listener and bind to an address
-    // we will await this until it is read to accept connection.
-    // unwrap() will panic if it cannot bind - i.e. the port is aleady in use.
-    let listener = TcpListener::bind("127.0.0.1:1338").await.unwrap();
+use tungstenite::{
+    accept_hdr,
+    handshake::server::{Request, Response},
+    WebSocket,
+};
 
-    // create a communication channel to send messages between clients and the server.
-    let (tx, _rx) = broadcast::channel(10);
+fn main() {
+    let server = TcpListener::bind("127.0.0.1:3012").unwrap();
 
-    loop {
-        // we destructure the socket and address from the listener after the connection is accepted.
-        // the connection may not be accepted if the listener is not ready to accept connections.
-        let (mut socket, addr) = listener.accept().await.unwrap();
+    // create an Arc<Mutex<HashMap>> of lobbies with a key of the lobby name and a value of connected clients
+    let lobbies: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
 
-        let tx = tx.clone();
-        let mut rx = tx.subscribe();
+    for stream in server.incoming() {
+        let lobbies_clone = Arc::clone(&lobbies);
+        spawn(move || {
+            let callback = |req: &Request, response: Response| {
+                println!("Received a new ws handshake");
+                println!("The request's path is: {}", req.uri().path());
 
-        tokio::spawn(async move {
-            // split the socket into a reader and writer.
-            let (reader, mut writer) = socket.split();
+                // add lobby to list
+                lobbies_clone
+                    .lock()
+                    .unwrap()
+                    .insert(req.uri().path().to_string(), "test".to_string());
 
-            // we create a buffer to store the incoming data.
-            let mut reader = BufReader::new(reader);
-            let mut line = String::new();
+                Ok(response)
+            };
 
-            println!("Connection established! {}", addr);
+            let mut websocket = accept_hdr(stream.unwrap(), callback).unwrap();
 
             loop {
-                tokio::select! {
-                result = reader.read_line(&mut line) => {
-
-                if result.unwrap() == 0 {
-                    break;
-                }
-                tx.send((line.clone(), addr)).unwrap();
-                line.clear();
-                }
-                result = rx.recv() => {
-                    let (msg, other_addr) = result.unwrap();
-
-                    if addr != other_addr {
-                        writer.write_all(msg.as_bytes()).await.unwrap();
-                    }
-                }
+                let msg = websocket.read_message().unwrap();
+                println!("Lobby list {:?}", lobbies_clone.lock().unwrap());
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
                 }
             }
         });
---
+++ src/main_tokio.rs
@@ -0,0 +1,62 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
+    println!("Connection starting!");
+    // create a TCP Listener and bind to an address
+    // we will await this until it is read to accept connection.
+    // unwrap() will panic if it cannot bind - i.e. the port is aleady in use.
+    let listener = TcpListener::bind("127.0.0.1:1338").await.unwrap();
+
+    // create a communication channel to send messages between clients and the server.
+    let (tx, _rx) = broadcast::channel(10);
+
+    loop {
+        // we destructure the socket and address from the listener after the connection is accepted.
+        // the connection may not be accepted if the listener is not ready to accept connections.
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            // split the socket into a reader and writer.
+            let (reader, mut writer) = socket.split();
+
+            // we create a buffer to store the incoming data.
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            println!("Connection established! {}", addr);
+
+            // read the first line to get the lobby name from the URL path
+            if reader.read_line(&mut line).await.unwrap() == 0 {
+                return; // connection closed
+            }
+
+            loop {
+                tokio::select! {
+                result = reader.read_line(&mut line) => {
+
+                if result.unwrap() == 0 {
+                    break;
+                }
+                tx.send((line.clone(), addr)).unwrap();
+                line.clear();
+                }
+                result = rx.recv() => {
+                    let (msg, other_addr) = result.unwrap();
+
+                    if addr != other_addr {
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                }
+                }
+            }
+        });
+    }
+}
