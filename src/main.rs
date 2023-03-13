c74b22e3278f57f72b9f06b78c831bb08a848c15
obarker94 <me@oliverbarker.io>
2023-03-13 15:28:48 +0000

    closing connections now handled

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
-// rewrite with tokio tungstenite
-
 use futures::SinkExt;
-use std::io::Error;
+use std::{collections::HashSet, io::Error, net::SocketAddr, sync::Arc};
 use tokio::{
     net::{TcpListener, TcpStream},
-    sync::broadcast,
+    sync::{broadcast, Mutex},
 };
 
 use futures_util::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     let try_socket = TcpListener::bind("127.0.0.1:8383").await;
     let listener = try_socket.expect("Failed to bind");
 
-    let (tx, _rx) = broadcast::channel::<String>(10);
+    // Create a Mutex-protected HashSet to store the connections
+    let connections: Arc<Mutex<HashSet<SocketAddr>>> = Arc::new(Mutex::new(HashSet::new()));
 
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
-                tokio::spawn(handle_connection(stream, tx.clone(), tx.subscribe()));
+                let connections = connections.clone();
+                tokio::spawn(handle_connection(
+                    stream,
+                    tx.clone(),
+                    tx.subscribe(),
+                    connections,
+                ));
             }
             Err(e) => {
                 println!("failed to accept connection: {}", e);
@@ -32,19 +39,33 @@ async fn handle_connection(
     stream: TcpStream,
     tx: broadcast::Sender<String>,
     mut rx: broadcast::Receiver<String>,
+    connections: Arc<Mutex<HashSet<SocketAddr>>>,
 ) {
+    // get the peer address
     let addr = stream
         .peer_addr()
         .expect("connected streams should have a peer address");
 
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
+    // add the peer address to the connections
+    {
+        let mut connections = connections.lock().await;
+        connections.insert(addr);
+        println!("Connection established: {} - List: {:?}", addr, connections);
+    }
+
+    // convert the TCPStream to a websocket
+    let ws_stream = match tokio_tungstenite::accept_async(stream).await {
+        Ok(ws_stream) => ws_stream,
+        Err(e) => {
+            println!("Error during the websocket handshake: {}", e);
+            return;
+        }
+    };
 
     let (mut write, mut read) = ws_stream.split();
 
     // send message to broadcast channel
-    tokio::spawn(async move {
+    let rx_task = tokio::spawn(async move {
         while let Some(msg) = read.next().await {
             let msg = msg.expect("Error during reading from websocket");
             tx.send(msg.to_string()).unwrap();
@@ -52,12 +73,40 @@ async fn handle_connection(
     });
 
     // read from broadcast channel and send to client
-    tokio::spawn(async move {
+    let tx_task = tokio::spawn(async move {
         while let Ok(msg) = rx.recv().await {
             let msg = tokio_tungstenite::tungstenite::Message::Text(msg.to_string());
             println!("Sending message {} to {}", msg, addr);
-            write.send(msg).await.unwrap();
-            // want to write here to socket but write doesnt have any send methods?
+            let sent_msg = write.send(msg).await;
+            match sent_msg {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error sending message to {}: {}", addr, e);
+                    // remove the peer address from the connections when the stream is closed
+                    let mut connections = connections.lock().await;
+                    connections.remove(&addr);
+                    println!("Connections: {:?}", connections);
+                    break;
+                }
+            }
         }
+    });
+
+    // remove the peer address from the connections when the stream is closed
+    tokio::spawn(async move {
+        tokio::select! {
+            result = rx_task => {
+                if let Err(e) = result {
+                    println!("Error reading from websocket: {:?}", e);
+                }
+            }
+            result = tx_task => {
+                if let Err(e) = result {
+                    println!("Error writing to websocket: {:?}", e);
+                }
+            }
+        }
+
+        println!("Connection closed: {}", addr);
     });
 }
