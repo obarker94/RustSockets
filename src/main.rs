fd6a8569f75fb9c06c81935a5339ab02587c2d50
obarker94 <me@oliverbarker.io>
2023-03-13 16:27:24 +0000

    lobbies created

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use futures::SinkExt;
-use std::{
-    collections::{HashMap, HashSet},
-    io::Error,
-    net::SocketAddr,
-    sync::Arc,
-};
+use std::{collections::HashMap, io::Error, net::SocketAddr, sync::Arc};
 use tokio::{
     net::{TcpListener, TcpStream},
     sync::{broadcast, Mutex},
@@ -12,15 +7,18 @@ use tokio::{
 
 use futures_util::StreamExt;
 
+#[derive(Debug, Clone)]
+struct LobbyMessage {
+    lobby: String,
+    message: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let (tx, _rx) = broadcast::channel::<String>(10);
+    let (tx, _rx) = broadcast::channel::<LobbyMessage>(10);
 
     let try_socket = TcpListener::bind("127.0.0.1:8383").await;
     let listener = try_socket.expect("Failed to bind");
-
-    // Create a Mutex-protected HashSet to store the connections
-    // let connections: Arc<Mutex<HashSet<SocketAddr>>> = Arc::new(Mutex::new(HashSet::new()));
 
     // Create a Mutex-protected HashMap to store a vector of connections against a lobby name
     let connections: Arc<Mutex<HashMap<String, Vec<SocketAddr>>>> =
@@ -30,11 +28,21 @@ async fn main() -> Result<(), Error> {
         match listener.accept().await {
             Ok((stream, _)) => {
                 let connections = connections.clone();
+
+                // get the lobby name from the url
+                let mut buf = [0; 1024];
+                let n = stream.peek(&mut buf).await.unwrap();
+                let path = String::from_utf8_lossy(&buf[..n]);
+                let path = path.split(" ").nth(1).unwrap();
+                let path = path.trim_start_matches("/").to_string();
+
+                let path = path.clone();
                 tokio::spawn(handle_connection(
                     stream,
                     tx.clone(),
                     tx.subscribe(),
                     connections,
+                    path,
                 ));
             }
             Err(e) => {
@@ -44,33 +52,29 @@ async fn main() -> Result<(), Error> {
     }
 }
 
-async fn handle_connection(
+async fn handle_connection<'a>(
     stream: TcpStream,
-    tx: broadcast::Sender<String>,
-    mut rx: broadcast::Receiver<String>,
+    tx: broadcast::Sender<LobbyMessage>,
+    mut rx: broadcast::Receiver<LobbyMessage>,
     connections: Arc<Mutex<HashMap<String, Vec<SocketAddr>>>>,
+    path: String,
 ) {
     // get the peer address
     let addr = stream
         .peer_addr()
         .expect("connected streams should have a peer address");
 
-    // get the lobby name from the url
-    let mut buf = [0; 1024];
-    let n = stream.peek(&mut buf).await.unwrap();
-    let path = String::from_utf8_lossy(&buf[..n]);
-    let path = path.split(" ").nth(1).unwrap();
-    let path = path.trim_start_matches("/");
     println!("Path: {}", path);
 
     // add the lobby (path) to the connections if it doesnt exist
     {
+        let path = path.clone();
         let mut connections = connections.lock().await;
-        if !connections.contains_key(path) {
+        if !connections.contains_key(path.as_str()) {
             connections.insert(path.to_string(), Vec::new());
         }
         connections
-            .get_mut(path)
+            .get_mut(path.as_str())
             .expect("Incorrect lobby was created or not found")
             .push(addr);
         println!("Connection established: {} - List: {:?}", addr, connections);
@@ -87,46 +91,64 @@ async fn handle_connection(
 
     let (mut write, mut read) = ws_stream.split();
 
+    let path = Arc::new(path.to_string());
+
     // send message to broadcast channel
-    let rx_task = tokio::spawn(async move {
-        while let Some(msg) = read.next().await {
-            let msg = msg.expect("Error during reading from websocket");
-            tx.send(msg.to_string()).unwrap();
+    let rx_task = tokio::spawn({
+        let path = path.clone(); // clone the Arc
+        async move {
+            while let Some(msg) = read.next().await {
+                let msg = msg.expect("Error during reading from websocket");
+                tx.send(LobbyMessage {
+                    lobby: path.to_string(),
+                    message: msg.to_string(),
+                })
+                .expect("Error sending message to broadcast channel");
+            }
         }
     });
 
     // read from broadcast channel and send to client
-    let tx_task = tokio::spawn(async move {
-        while let Ok(msg) = rx.recv().await {
-            let msg = tokio_tungstenite::tungstenite::Message::Text(msg.to_string());
-            println!("Sending message {} to {}", msg, addr);
-            let sent_msg = write.send(msg).await;
-            match sent_msg {
-                Ok(_) => {}
-                Err(e) => {
-                    println!("Error sending message to {}: {}", addr, e);
+    let tx_task = tokio::spawn({
+        let path = path.clone(); // clone the Arc
+        async move {
+            while let Ok(msg) = rx.recv().await {
+                let tokio_msg = tokio_tungstenite::tungstenite::Message::Text(msg.message);
 
-                    // remove the peer address from the vector of connections of the connected
-                    // lobby when the stream is closed
-                    let mut connections = connections.lock().await;
-                    let lobby = connections
-                        .iter()
-                        .find(|(_, v)| v.contains(&addr))
-                        .expect("Lobby not found")
-                        .0
-                        .to_string();
-                    connections
-                        .get_mut(&lobby)
-                        .expect("Incorrect lobby was created or not found")
-                        .retain(|&x| x != addr);
+                // only recieve messages from the connected lobby (path)
+                if msg.lobby != *path {
+                    continue;
+                }
+                println!("Sending message {} to {}", tokio_msg, addr);
+                let sent_msg = write.send(tokio_msg).await;
 
-                    // if no connections are in lobby then remove the lobby
-                    if connections.get(&lobby).unwrap().len() == 0 {
-                        connections.remove(&lobby);
+                match sent_msg {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error sending message to {}: {}", addr, e);
+
+                        // remove the peer address from the vector of connections of the connected
+                        // lobby when the stream is closed
+                        let mut connections = connections.lock().await;
+                        let lobby = connections
+                            .iter()
+                            .find(|(_, v)| v.contains(&addr))
+                            .expect("Lobby not found")
+                            .0
+                            .to_string();
+                        connections
+                            .get_mut(&lobby)
+                            .expect("Incorrect lobby was created or not found")
+                            .retain(|&x| x != addr);
+
+                        // if no connections are in lobby then remove the lobby
+                        if connections.get(&lobby).unwrap().len() == 0 {
+                            connections.remove(&lobby);
+                        }
+                        println!("Connections: {:?}", connections);
+
+                        break;
                     }
-                    println!("Connections: {:?}", connections);
-
-                    break;
                 }
             }
         }
